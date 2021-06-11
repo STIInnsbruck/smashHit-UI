56e58ff5bead40e4bc5d5b13f46d348a2d4cb231
svencarstenrasmusen <sven.the.eagle@gmail.com>
2021-06-11 11:24:31 +0200

    added contract and contractObject model

--- lib/data/models.dart
+++ lib/data/models.dart
@@ -12,3 +12,32 @@ class User {
   
   set setName(String value) { _name = value; }
 }
+
+/// Contract model. There is always one party creating the contract (= contractor)
+/// and there can be 1 or more parties that accept the contract (= contractee).
+/// If the contract is supposed to only be an instant contract lasting for a day,
+/// then make the startDate and endDate the same date.
+class Contract {
+  User contractor;
+  List<User> contractee = [];
+  ContractObject contractObject;
+  String title;
+  String description;
+  DateTime startDate;
+  DateTime endDate;
+}
+
+
+/// Object model to be used in a Contract. It describes the actual object
+/// (fictive or physical) that is handled in the contract.
+class ContractObject {
+  String _description;
+
+  ContractObject(String description) {
+    this._description = description;
+  }
+
+  String get getDescription => _description;
+
+  set setDescription(String description) { _description = description; }
+}
