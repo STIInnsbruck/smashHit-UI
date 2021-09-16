cc62fc96d7549b854bcbf8881176e1652543cfc5
svencarstenrasmusen <sven.the.eagle@gmail.com>
2021-09-16 14:14:12 +0200

    new web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -12,8 +12,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7aef0510615efed4a5db4a3b0ccd2b0a",
-"/": "7aef0510615efed4a5db4a3b0ccd2b0a",
+"index.html": "2867c15ed8b30b096491150f302c94aa",
+"/": "2867c15ed8b30b096491150f302c94aa",
 "main.dart.js": "bf25ba4ef99972026201d6cb9c37ecc6",
 "manifest.json": "cfec03e163b7dee33052c1e3ad08d671",
 "version.json": "86e0beb60b1b72999178715f0d69ed97"
