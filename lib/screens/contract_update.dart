707f411eb5912b0ed3c6d42a925ac8c2682a1fb4
svencarstenrasmusen <sven.the.eagle@gmail.com>
2021-10-05 13:10:19 +0200

    added initial classes

---
+++ lib/screens/contract_update.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UpdateScreen extends StatefulWidget {
+
+  @override
+  _UpdateScreenState createState() => new _UpdateScreenState();
+}
+
+class _UpdateScreenState extends State<UpdateScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
