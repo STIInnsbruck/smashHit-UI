4ecb8def85b15e832942cdb24c9055eccf2d7350
SvenRasmusen <sven@home.lu>
2022-03-24 13:43:12 +0100

    renamed file name

--- lib/screens/base_page.dart
+++ lib/screens/base_page.dart
@@ -8,7 +8,7 @@ import 'package:smashhit_ui/screens/template_selector.dart';
 import 'package:smashhit_ui/screens/contract_violation.dart';
 import 'package:smashhit_ui/screens/login.dart';
 import 'package:smashhit_ui/screens/contract_update.dart';
-import 'package:smashhit_ui/screens/profile_manager.dart';
+import 'package:smashhit_ui/screens/profile_editor.dart';
 import 'package:smashhit_ui/screens/contract_party_profile.dart';
 
 class BasePage extends StatefulWidget {
