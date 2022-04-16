6a8e3702cbed026b0ee063817c5bebda4ac9785b
SvenRasmusen <sven@home.lu>
2022-04-16 22:55:53 +0200

    added contract types and categories list in separate file

---
+++ lib/misc/contract_types.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+List<DropdownMenuItem<String>> contractTypes = [
+  DropdownMenuItem(child: Text("Written Contract"),value: "Written"),
+  DropdownMenuItem(child: Text("Verbal Contract"),value: "Verbal"),
+  DropdownMenuItem(child: Text("Mutual Contract"),value: "Mutual"),
+  DropdownMenuItem(child: Text("Transferable Contract"),value: "Transferable"),
+];
