b523fb54e35dce8ab6dc3d6f7b2c7c8b4d2607f5
SvenRasmusen <sven@home.lu>
2022-10-04 15:43:50 +0200

    new web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7215feedc9ce5684888f31106a2887aa",
-"/": "7215feedc9ce5684888f31106a2887aa",
-"main.dart.js": "8cad16eea3f3a5391ac7c2907c6d0c81",
+"index.html": "c76189f77d60ecd287955b9b5ec11d53",
+"/": "c76189f77d60ecd287955b9b5ec11d53",
+"main.dart.js": "a83207dca8e073a40e1869d07e1f77dd",
 "manifest.json": "cfec03e163b7dee33052c1e3ad08d671",
 "version.json": "889654efd90cc87fd49cb6e39cb54ba7"
 };
